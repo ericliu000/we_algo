44af546ea41f62eedc003417dd3a0618bc4eb800
印晓 <liyinxiao@51shebao.com>
2021-04-22 23:36:33 +0800

    ignore

--- php/sort/StringHelper.php
+++ php/sort/StringHelper.php
@@ -6,5 +6,17 @@ namespace algo\sort;
 
 class StringHelper
 {
+    private function __construct()
+    {
+    }
 
+    public static function isSort(array $arr): bool
+    {
+        for ($i=1;$i<count($arr);$i++) {
+            if ($arr[$i-1] > $arr[$i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
