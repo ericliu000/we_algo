23d124ce394ed973491075ab51f6f05e0466b0d4
李印晓 <liyinxiao@51shebao.com>
2021-05-16 17:03:47 +0800

    remove linkedlist ele

--- src/leetcode/I0203RemoveLinkedListElements/Solution.php
+++ src/leetcode/I0203RemoveLinkedListElements/Solution.php
@@ -3,8 +3,50 @@
 
 namespace algo\leetcode\I0203RemoveLinkedListElements;
 
+use algo\leetcode\common\ListNode;
 
-class Solution
-{
+/**
+ * @link https://class.imooc.com/lesson/1580#mid=36140
+ *
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
 
+    /**
+     * @param ListNode $head
+     * @param Integer $val
+     * @return ListNode
+     */
+    function removeElements($head, $val) {
+
+        // 如果头结点命中
+        while ($head != null && $head->val == $val) {
+            $head = $head->next;
+        }
+
+        if ($head == null) {
+            return null;
+        }
+
+        // 如果非头结点命中
+        $prev = $head;
+        while ($prev->next != null) {
+            /** @var ListNode $cur */
+            if ($prev->next->val == $val) {
+                $prev->next = $prev->next->next;
+            } else {
+                $prev = $prev->next;
+            }
+        }
+
+        return $head;
+    }
 }
--- tests/leetcode/0203/SolutionTestTest.php
+++ tests/leetcode/0203/SolutionTestTest.php
@@ -1,9 +1,45 @@
 <?php
 
 
+use algo\leetcode\common\LinkedListUtil;
+use algo\leetcode\I0203RemoveLinkedListElements\Solution;
 use PHPUnit\Framework\TestCase;
 
 class SolutionTestTest extends TestCase
 {
 
+    public function provider()
+    {
+        return [
+            [
+                'expected' => [],
+                'listNode' => [],
+                'remove' => 1,
+            ],
+            [
+                'expected' => [1, 2, 3, 4, 5],
+                'listNode' => [1, 2, 6, 3, 4, 5, 6],
+                'remove' => 6,
+            ],
+            [
+                'expected' => [],
+                'listNode' => [7, 7, 7, 7],
+                'remove' => 7,
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     * @param $expected
+     * @param $listNode
+     * @param $remove
+     */
+    public function testRemoveElements($expected, $listNode, $remove)
+    {
+        $sol = new Solution();
+        $list = LinkedListUtil::creator($listNode);
+        $actual = $sol->removeElements($list, $remove);
+        $this->assertEquals($expected, LinkedListUtil::parser($actual));
+    }
 }
