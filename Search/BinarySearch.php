0d5f3fd9bf229fe481aed66c01f3a0912db4e042
yanhui <237890221@qq.com>
2021-06-01 08:51:40 +0800

    递归二分查找

--- Search/BinarySearch.php
+++ Search/BinarySearch.php
@@ -10,5 +10,38 @@
  */
 class BinarySearch
 {
+    public $arr = [1,2,3,4,5,6,7,8];
 
+    public $target = 6;
+
+
+    public function run(){
+        return $this->search(0,count($this->arr)-1);
+    }
+
+
+    /**
+     * 二分查找法-递归
+     *
+     * @param $left
+     * @param $right
+     * @return float|int
+     */
+    public function search($left,$right){
+        if($left > $right) return -1;
+
+        $mid = floor(($right + $left) / 2);
+        if($this->arr[$mid] > $this->target){
+            $this->search($left,$mid-1);
+        }elseif($this->arr[$mid] < $this->target){
+            $this->search($mid + 1,$right);
+        }else{
+            return $mid;
+        }
+
+        echo json_encode([$left,$right,$mid]) . PHP_EOL;
+    }
 }
+
+$binarySearch = new BinarySearch();
+echo $binarySearch->run() . PHP_EOL;
